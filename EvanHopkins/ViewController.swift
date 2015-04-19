c4e54e52419848cc4abd04ee7eb0ab6ee004845d
popcat <moviemaker7@gmail.com>
2015-04-18 20:29:42 -0400

    Initial Commit

---
+++ EvanHopkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EvanHopkins
+//
+//  Created by Evan Hopkins on 4/18/15.
+//  Copyright (c) 2015 Evan Hopkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
